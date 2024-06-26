d78e87e684ff4f7c9d05165cf20d12570ee46a26
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-06-26 20:05:25 +0300

    install the notion logo and start working on the navbar component

---
+++ vue3-client/src/components/TheNav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-20 bg-white flex items-center justify-between">
+    <div>
+      <the-logo />
+      <router-link class="" to="/">Home</router-link>
+      <router-link class="" to="/">Home</router-link>
+      <router-link class="" to="/">Home</router-link>
+    </div>
+    <nav>
+      <router-link class="" to="/login">Login</router-link>
+      <router-link class="" to="/dashboard">Try Notion free</router-link>
+    </nav>
+  </div>
+</template>
+<script>
+import TheLogo from '@/components/TheLogo.vue'
+export default {
+  name: 'TheNav',
+  components: {
+    TheLogo
+  }
+}
+</script>
--- vue3-client/src/layouts/DefaultLayout.vue
+++ vue3-client/src/layouts/DefaultLayout.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Default Layout</h1>
-  <slot />
+  <div class="h-screen max-w-screen-xl bg-blue-300 mx-auto">
+    <the-nav />
+    <slot />
+  </div>
 </template>
 <script>
+import TheNav from '@/components/TheNav.vue'
 export default {
-  name: 'DefaultLayout'
+  name: 'DefaultLayout',
+  components: {
+    TheNav
+  }
 }
 </script>
